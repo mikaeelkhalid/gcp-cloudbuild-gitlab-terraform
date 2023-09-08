db936a972b4b5a256f8b97d0c3acd94df4504b82
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-09-08 05:18:36 +0200

    add gitlab connection

--- cloud-build.tf
+++ cloud-build.tf
@@ -9,3 +9,18 @@ resource "google_project_iam_member" "cloudbuild_secret_accessor" {
   role    = "roles/secretmanager.secretAccessor"
   member  = "serviceAccount:PROJECT_NUMBER@cloudbuild.gserviceaccount.com"
 }
+
+resource "google_cloudbuildv2_connection" "gitlab-connection" {
+  location = var.build_location
+  name     = "gitlab-connection"
+  gitlab_config {
+    authorizer_credential {
+      user_token_secret_version = var.gitlab_api_token_secret
+    }
+    read_authorizer_credential {
+      user_token_secret_version = var.gitlab_read_api_token_secret
+    }
+    webhook_secret_secret_version = var.gitlab_webhook_token_secret
+  }
+}
+
