51ed5c603e9b36eb06ceac0d299f4c3d579fb043
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-09-08 05:16:03 +0200

    add variables

--- variables.tf
+++ variables.tf
@@ -9,3 +9,29 @@ variable "gcp_region" {
   default     = "us-central1"
   description = "The name of the Google region where the cloud build are to be provisioned"
 }
+
+variable "gitlab_api_token_secret" {
+  type        = string
+  description = "GitLab API Token Secret"
+}
+
+variable "gitlab_read_api_token_secret" {
+  type        = string
+  description = "GitLab Read API Token Secret"
+}
+
+variable "gitlab_webhook_token_secret" {
+  type        = string
+  description = "GitLab Webhook Token Secret"
+}
+
+variable "gitlab_repo_uri" {
+  type        = string
+  description = "GitLab Repo URI"
+}
+
+variable "build_location" {
+  type        = string
+  default     = var.gcp_region
+  description = "Cloud Build Location"
+}
